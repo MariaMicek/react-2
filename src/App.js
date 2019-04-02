8d70e7e475ef48b31f1ecbd0f208fd3c9a94a322
Maria Micek <mmicek16@wp.pl>
2019-04-02 10:22:14 +0200

    Nested components with props - App.js, Header.js, Name.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
 import React from 'react'
+import Header from './Header'
 
 const App = () => {
     return (
-        <h1> Hello World </h1>
+        <Header 
+            firstName = {'Marysia'}
+            lastName = {'Micek'}
+        />
     )
 }
 

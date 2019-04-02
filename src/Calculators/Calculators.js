e6bb04ae358cba3c444b2b8f8b9f66e43508edc9
Maria Micek <mmicek16@wp.pl>
2019-04-02 11:58:21 +0200

    new folders: Home, Calculator and Calculators, default exports in index.js in each folder

---
+++ src/Calculators/Calculators.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Calculator from '../Calculator'
+
+const numbers = [
+    { a: 5, b: 10 },
+    { a: 7, b: 12 },
+    { a: 3, b: 11 },
+];
+
+const Calculators = () => {
+    return (
+        numbers.map(el =>
+            <Calculator
+                a={el.a}
+                b={el.b}
+            />
+        )
+    )
+}
+
+export default Calculators
